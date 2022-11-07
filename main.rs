890a778866ea1d04ad530a987798f292496ec939
idanai <39054053+idanai@users.noreply.github.com>
2022-11-07 14:34:19 +0200

    Simplification and Style
    The feature for chaining dice (e.g 1d4d6...) feels pointless and makes the code...Too much.
    Also prints the results in a nice table with average for each dice group as well.

--- main.rs
+++ main.rs
@@ -1,101 +1,126 @@
-use rand::prelude::*;
-
-type Int = u32;
-
-struct DiceRoll {
-	num : Int,
-	die : Int,
-}
-
-impl DiceRoll {
-	pub fn new(num: Int, die: Int) -> Self {
-		Self{num, die}
+mod dice {
+	use std::num::ParseIntError;
+	// Errors that may occure when parsing Dice
+	pub enum Error{
+		Parse,
+		MoreThanOneD(Dice), // still parses the first Dice
+		NoD,
 	}
 
-	pub fn roll(&self) -> Int {
-		if self.always_zero() {
-			return 0;
+	impl From<ParseIntError> for Error {
+		fn from(_: ParseIntError) -> Self{
+			Self::Parse
 		}
-		let mut rng = rand::thread_rng();
-		(0..self.num).map(|_| rng.gen_range(1..=self.die)).sum()
 	}
 
-	pub fn always_zero(&self) -> bool {
-		self.num == 0 || self.die == 0
+	// Contains two numbers, the first: a number of dice, the second: number of faces on each die.
+	// can be used to roll dice with an external random number generator
+	#[derive(Clone, Copy)]
+	pub struct Dice {
+		pub num: u32,
+		pub die: u32,
 	}
 
-	fn util_push_die(&mut self, n: Int) {
-		self.num = std::mem::replace(&mut self.die, n);
-	}
-}
+	impl Dice {
+		// returns a lazy iterator over @self.num randomly generated numbers in a range [1, @self.die]
+		pub fn roll<'a>(&self, rng: &'a mut rand::rngs::ThreadRng) -> impl Iterator<Item = u32> + 'a {
+			use rand::Rng;
+			let n = if self.always_zero() {0} else {self.num};
 
-#[derive(Debug)]
-enum DiceRollParseError {
-	Error
-}
-
-impl From<std::num::ParseIntError> for DiceRollParseError {
-	fn from(_: std::num::ParseIntError) -> Self {
-			Self::Error // Self::ParseIntError
-	}
-}
-
-impl std::str::FromStr for DiceRoll {
-	type Err = DiceRollParseError;
-
-	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		let mut numbers = s.split(&['d', 'D']);
-
-		let mut dice = DiceRoll {
-			num: match numbers.next() {
-				Some(s) => if s.is_empty() {1} else { s.parse()? },
-				_ => return Err(Self::Err::Error)
-			},
-			die: match numbers.next() {
-				Some(s) => s.parse()?,
-				_ => return Err(Self::Err::Error)
-			}
-		};
-
-		while let Some(n_str) = numbers.next() {
-			let roll = dice.roll();
-			// if the number of dice is zero at any iteration, jump to the last iteration
-			if roll == 0 {
-				return Ok(DiceRoll::new(0, numbers.last().unwrap().parse()?));
-			}
-			dice.die = roll;
-			dice.util_push_die(n_str.parse()?);
+			let dice = *self;
+			(0..n).map(move |_| rng.gen_range(1..=dice.die))
 		}
 
-		Ok(dice)
+		// test if the result of rolling Dice always returns 0
+		pub fn always_zero(&self) -> bool {
+			self.num == 0 || self.die == 0
+		}
+	}
+
+	// Can parse the following format: <Optional uint><'d' or 'D'><uint>
+	// Examples: d4, 2d6, 1d8, 0d10, 12d0
+	impl std::str::FromStr for Dice {
+		type Err = Error;
+
+		fn from_str(s: &str) -> Result<Self, Self::Err> {
+			let mut numbers = s.split(['d', 'D']);
+
+			let mut next = || -> Result<&str, Error> {
+				numbers.next().ok_or(Error::NoD)
+			};
+
+	//		let num = {
+	//			let s = next()?;
+	//			Result::<u32, Error>::Ok( if s.is_empty() {1} else {s.parse()?} )
+	//		}?;
+			let s = next()?;
+
+			let dice = Dice {
+				num: if s.is_empty() {1} else {s.parse()?},
+				die: next()?.parse()?
+			};
+
+			// expecting a single 'd'
+			if numbers.next().is_some() {
+				return Err(Error::MoreThanOneD(dice));
+			}
+
+			Ok(dice)
+		}
 	}
 }
 
-fn main() {
-	let args : Vec<String> = std::env::args().collect();
-	
+fn main(){
+	use dice::*;
+	use termion::{style, color};
+
+	let mut args: Vec<String> = std::env::args().collect();
+
 	if args.len() == 1 {
-		println!("Try \"{} help\"", args[0]);
+		println!("Try {} help", args[0]);
 		return;
 	}
 
+	args[1].make_ascii_lowercase();
 	if args[1].contains("help") {
 		println!(
-"Welcom to the dice rolling program '{program_name}' (made by Idany))!
-Syntax: <optional number><'d' or 'D'><number><optional sequence of repeateing <'d' or 'D'><number>>
-Examples:
-	d4 = rolls a 4 sided die
-	1d20 = rolls a 20 sided die
-	2d6 = rolls two 6 sided dice
-	1d6d4 = rolls a 6 sided die to determine how many 4 sided dice to roll (can concatenate as many dice as you like this way)
-Type the following example in your CLI to see all possible syntaxes: {program_name} d20 1d8 2d6 1d6d4", program_name = args[0]);
+"Generates random-number-sequences in a dice rolling format.
+Test by entering: {} d4 2d6 0d10 20d0", args[0]);
 		return;
 	}
 
+	let mut rng = rand::thread_rng(); // random number generator
+
+	println!("{}  Dice\t╬  Sum\t╬  Avg\t╬  Rolls{}", style::Bold, style::Reset); // table header
+
 	for arg in &args[1..] {
-		match arg.parse::<DiceRoll>() {
-			Ok(dice) => println!("{}....{}", dice.roll(), arg),
-			Err(e) => println!("{:?}....{}", e, arg)
+		match arg.parse::<Dice>() {
+			Ok(dice) => {
+				let rolls: Vec<u32> = dice.roll(&mut rng).collect();
+				let sum: u32 = rolls.iter().sum();
+
+				print!("{arg}\t│{bold}{dye}{sum}{reset}\t│",
+					arg = arg,
+					bold = style::Bold,
+					dye = color::Fg(color::Cyan),
+					reset = style::Reset,
+					sum = sum,
+				);
+
+				if rolls.len() > 1 {
+					let avg = if sum == 0 {0f32} else {sum as f32 / rolls.len() as f32};
+					print!("{:.2}\t│{:?}", avg, rolls);
+				}else{
+					print!("\t│");
+				}
+				println!();
+			}
+			Err(_) => println!("{fg}{bold}{arg}{reset}\t│{bg}FAIL{reset}\t│\t│",
+				arg=arg,
+				bold=style::Bold,
+				bg=color::Bg(color::Red),
+				fg=color::Fg(color::Red),
+				reset=style::Reset)
 		}
 	}
 }
